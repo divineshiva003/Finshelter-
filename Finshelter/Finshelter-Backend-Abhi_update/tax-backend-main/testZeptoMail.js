1d1d66b9a6c8978189ea3fc3f2f6a77445852dc3
SHIVAM KUMAR RAI <128404499+divineshiva003@users.noreply.github.com>
2025-12-26 23:12:29 +0530

    Delete Finshelter directory

--- Finshelter/Finshelter-Backend-Abhi_update/tax-backend-main/testZeptoMail.js
+++
@@ -1,9 +0,0 @@
-const sendZeptoMail = require('./utils/sendZeptoMail');
-
-sendZeptoMail({
-    to: 'superadmin@thefinshelter.com',
-    subject: 'Test Email',
-    html: 'Test email sent successfully.'
-})
-.then(info => console.log('Successfully sent:', info))
-.catch(err => console.error('Error sending email:', err));
--- Finshelter/Finshelter_Frontend-Abhi_frontend/Finshelter_Frontend/src/Admin/utils/axiosConfig.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const axiosConfig = axios.create({
-	baseURL: import.meta.env.VITE_API_URL 
-		? `${import.meta.env.VITE_API_URL.replace(/\/$/, '')}/api`
-		: "http://localhost:8000/api",
-});
-
-export default axiosConfig;
